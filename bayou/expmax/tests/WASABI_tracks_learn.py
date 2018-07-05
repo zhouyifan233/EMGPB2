1e48c7bdd7ea6f8fe649350e041350e80dd29c3f
YIFAN ZHOU <zhouyifan1990@gmail.com>
2018-07-05 15:26:20 +0100

    Fixed H and fixed R... looks fine?

--- bayou/expmax/tests/WASABI_tracks_learn.py
+++ bayou/expmax/tests/WASABI_tracks_learn.py
@@ -9,13 +9,13 @@ from bayou.expmax.lineargaussian import LinearGaussian
 def test_em():
     """ """
     dataset = []
-    for i in range(1, 2):
-        measurement = np.loadtxt("C:\\WPAFB-learn-dynamics\\data_global_xy\\track_%04d.csv" % i, delimiter=',')
+    for i in range(1, 101):
+        measurement = np.loadtxt("E:\\WPAFB-learn-dynamics\\data_global_xy\\track_%04d.csv" % i, delimiter=',')
         for_init_state = measurement[0,:].tolist()
         for_init_state.extend([0, 0])
         initial_state = Gaussian(np.array(for_init_state).reshape(4,1), 1*np.eye(4))
         
-        initial_model = ConstantVelocity(1.0, 25.0, 9.0, 4, 2)
+        initial_model = ConstantVelocity(1.0, 100.0, 4.0, 4, 2)
         measurement = np.expand_dims(measurement, axis=-1)
         sequence = GaussianSequence(measurement, initial_state)
         dataset.append(sequence)
@@ -24,7 +24,7 @@ def test_em():
 
     model, dataset, LLs = LinearGaussian.EM(dataset, initial_model,
                                             max_iters=100, threshold=0.0001,
-                                            learn_H=True, learn_R=True,
+                                            learn_H=False, learn_R=False,
                                             learn_A=True, learn_Q=True, learn_init_state=False,
                                             keep_Q_structure=False, diagonal_Q=False)
 
--- bayou/expmax/tests/test_em_lineargaussian.py
+++ bayou/expmax/tests/test_em_lineargaussian.py
@@ -11,13 +11,13 @@ def test_em():
     initial_state = Gaussian(np.ones([2, 1]), np.eye(2))
     initial_model = LinearModel(np.eye(2), np.eye(2), np.eye(2), 0.01 * np.eye(2))
 
-    measurements = np.loadtxt('measurements.csv', delimiter=',')
+    measurements = np.loadtxt('C:\\Users\\yifan\\source\\repos\\bayou\\bayou\\expmax\\tests\\measurements.csv', delimiter=',')
     measurements = np.expand_dims(measurements, axis=-1)
     sequence = GaussianSequence(measurements, initial_state)
     dataset = [sequence]
 
     model, dataset, LLs = LinearGaussian.EM(dataset, initial_model,
-                                            max_iters=5, threshold=0.0001,
+                                            max_iters=100, threshold=0.000001,
                                             learn_H=True, learn_R=True,
                                             learn_A=True, learn_Q=True, learn_init_state=True,
                                             keep_Q_structure=False, diagonal_Q=False)
