d15a5cb29988b831c4fb95c0d5c2f9ac90a41a45
YIFAN ZHOU <zhouyifan1990@gmail.com>
2018-07-17 21:05:00 +0100

    consider grayscale

--- bayou/expmax/tests/WASABI_tracks_learn.py
+++ bayou/expmax/tests/WASABI_tracks_learn.py
@@ -9,13 +9,13 @@ from bayou.expmax.lineargaussian import LinearGaussian
 def test_em():
     """ """
     dataset = []
-    for i in range(1, 101):
-        measurement = np.loadtxt("E:\\WPAFB-learn-dynamics\\data_global_xy\\track_%04d.csv" % i, delimiter=',')
+    for i in range(1, 2001):
+        measurement = np.loadtxt("E:\\WPAFB-learn-dynamics\\grayscale\\track_%04d.csv" % i, delimiter=',')
         for_init_state = measurement[0,:].tolist()
-        for_init_state.extend([0, 0])
-        initial_state = Gaussian(np.array(for_init_state).reshape(4,1), 1*np.eye(4))
+        #for_init_state.extend([0, 0])
+        initial_state = Gaussian(np.array(for_init_state).reshape(10,1), 1*np.eye(10))
         
-        initial_model = ConstantVelocity(1.0, 1.0, 1.0, 4, 2)
+        initial_model = LinearModel(np.eye(10), 1*np.eye(10), np.eye(10), 10*np.eye(10))
         measurement = np.expand_dims(measurement, axis=-1)
         sequence = GaussianSequence(measurement, initial_state)
         dataset.append(sequence)
@@ -23,19 +23,31 @@ def test_em():
             print("File %d loaded ..." % i)
 
     model, dataset, LLs = LinearGaussian.EM(dataset, initial_model,
-                                            max_iters=100, threshold=0.0001,
-                                            learn_H=False, learn_R=False,
-                                            learn_A=True, learn_Q=True, learn_init_state=False,
+                                            max_iters=100, threshold=0.00000001,
+                                            learn_H=True, learn_R=True,
+                                            learn_A=False, learn_Q=True, learn_init_state=False,
                                             keep_Q_structure=False, diagonal_Q=False)
 
     print("A: ")
-    print(model.A)
+    printarray = model.A
+    printarray = np.array2string(printarray, max_line_width=1000)
+    print(printarray)
+    #print(model.A)
     print("Q: ")
-    print(model.Q)
+    printarray = model.Q
+    printarray = np.array2string(printarray, max_line_width=1000)
+    print(printarray)
+    #print(model.Q)
     print("H: ")
-    print(model.H)
+    printarray = model.H
+    printarray = np.array2string(printarray, max_line_width=1000)
+    print(printarray)
+    #print(model.H)
     print("R: ")
-    print(model.R)
+    printarray = model.R
+    printarray = np.array2string(printarray, max_line_width=1000)
+    print(printarray)
+    #print(model.R)
     print("List of log-likelihood: ")
     print(LLs)
     #print("initial state mean: ")
