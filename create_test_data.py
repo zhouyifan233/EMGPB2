6181a3604efdf8ac4c60cc2dd416e6054cffa0b2
YIFAN ZHOU <zhouyifan1990@gmail.com>
2019-12-09 12:14:14 +0000

    update

--- create_test_data.py
+++ create_test_data.py
@@ -1,6 +1,74 @@
 import numpy as np
 import scipy as sp
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
+def get_Q(Q_sig, dt=1):
+    Q = Q_sig * np.asarray([
+        [(1/3)*np.power(dt, 3), 0, (1/2)*np.power(dt, 2), 0],
+        [0, (1/3)*np.power(dt, 3), 0, (1/2)*np.power(dt, 2)],
+        [(1/2) * np.power(dt, 2), 0, dt, 0],
+        [0, (1/2) * np.power(dt, 2), 0, dt]
+    ])
+    return Q
 
 
+def get_R(R_sig):
+    R = R_sig * np.eye(2)
+    return R
+
+# Time
+t = 200
+# transformation matrix
+F = np.asarray([
+    [1, 0, 1, 0],
+    [0, 1, 0, 1],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]
+])
+# measurement model
+H = np.asanyarray([
+    [1, 0, 0, 0],
+    [0, 1, 0, 0]
+])
+# Let's assume two Kalman filters
+Q = [get_Q(0.1), get_Q(0.01)]
+R = [get_R(1), get_R(1)]
+
+# Create path
+kf_ind = 0
+kf_change_pnt = [600]
+x_tminus1 = np.asarray([[0], [0], [1], [1]])
+path = []
+meas = []
+for i in range(t):
+    x_t_ = F @ x_tminus1
+    x_t = np.random.multivariate_normal(np.squeeze(x_t_), Q[kf_ind])
+    x_t = x_t.reshape((4, 1))
+    x_tminus1 = x_t
+    y_t_ = H @ x_t
+    y_t = np.random.multivariate_normal(np.squeeze(y_t_), R[kf_ind])
+    y_t = y_t.reshape((2, 1))
+    if i in kf_change_pnt:
+        kf_ind += 1
+    path.append(x_t)
+    meas.append(y_t)
+path = np.squeeze(np.asarray(path))
+meas = np.squeeze(np.asarray(meas))
+plt.figure()
+plt.plot(meas[:,0], meas[:,1], 'r-o', label='measurements')
+plt.plot(path[:,0], path[:,1], 'b-o', label='True trajectory')
+plt.legend()
+
+print('F:')
+print(F)
+print('H:')
+print(H)
+for i in range(len(kf_change_pnt)+1):
+    print('Q_' + str(i) + ': ')
+    print(Q[i])
+    print('R_' + str(i) + ': ')
+    print(R[i])
+data = pd.DataFrame(meas)
+data.to_csv('data/measurement1.csv', index=False, header=False)
--- test_em_lineargaussian.py
+++ test_em_lineargaussian.py
@@ -7,28 +7,47 @@ from bayou.expmax.lineargaussian import LinearGaussian
 
 
 def test_em():
-    """ """
-    initial_state = Gaussian(np.ones([2, 1]), np.eye(2))
-    initial_model = LinearModel(np.eye(2), np.eye(2), np.eye(2), 0.01 * np.eye(2))
+    F = np.asarray([
+        [1, 0, 1, 0],
+        [0, 1, 0, 1],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+    H = np.asanyarray([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0]
+    ])
+    """
+    Gaussian(mean, covariance)
+    LinearModel(A, Q, H, R)
+    """
+    initial_state = Gaussian(np.zeros([4, 1]), np.eye(4))
+    initial_model = LinearModel(F, 1*np.eye(4), H, 1*np.eye(2))
 
-    measurements = np.loadtxt('measurements.csv', delimiter=',')
+    measurements = np.loadtxt('data/measurement1.csv', delimiter=',')
     measurements = np.expand_dims(measurements, axis=-1)
     sequence = GaussianSequence(measurements, initial_state)
     dataset = [sequence]
 
     model, dataset, LLs = LinearGaussian.EM(dataset, initial_model,
-                                            max_iters=5, threshold=0.0001,
-                                            learn_H=True, learn_R=True,
-                                            learn_A=True, learn_Q=True, learn_init_state=True,
+                                            max_iters=100, threshold=1e-8,
+                                            learn_H=False, learn_R=True,
+                                            learn_A=False, learn_Q=True, learn_init_state=True,
                                             keep_Q_structure=False, diagonal_Q=False)
 
+    print('F: ')
     print(model.A)
+    print('Q: ')
     print(model.Q)
+    print('H: ')
     print(model.H)
+    print('R: ')
     print(model.R)
+    print('LLs: ')
     print(LLs)
+    print('Init_state: ')
     print(dataset[0].initial_state.mean)
-    print(dataset[0].initial_state.covar)
+    #print(dataset[0].initial_state.covar)
 
 
 test_em()
